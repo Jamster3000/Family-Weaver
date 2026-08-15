472623fe8afe6296adc12651d64785fd334b1b4c
jamster <148760154+Jamster3000@users.noreply.github.com>
2026-08-15 11:00:45 +0100

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import '../app.css';
+    import { navigating } from '$app/stores';
+    import { fade } from 'svelte/transition';
+</script>
+
+{#key $navigating?.to?.url.pathname}
+  <div in:fade={{ duration: 550 }}>
+    <slot />
+  </div>
+{/key}

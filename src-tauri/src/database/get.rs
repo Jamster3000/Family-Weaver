f8b00e3d8fffab1ed648b39efe2e399e69d916ea
jamster3000 <jrbaines.04@gmail.com>
2026-08-19 11:39:25 +0100

    Made getting the active tree SQL query to be optional so that it didn't return no rows error at the start of a new database

--- src-tauri/src/database/get.rs
+++ src-tauri/src/database/get.rs
@@ -1,23 +1,27 @@
 use crate::state::AppState;
 use crate::models::tree::Tree;
+use rusqlite::OptionalExtension;
 
 #[tauri::command]
-pub async fn get_active_tree(state: tauri::State<'_, AppState>) -> Result<Tree, String> {
-	let conn = state.conn.lock().map_err(|e| e.to_string())?;
+pub async fn get_active_tree(state: tauri::State<'_, AppState>) -> Result<Option<Tree>, String> {
+    let conn = state.conn.lock().map_err(|e| e.to_string())?;
 
-	let tree_data = conn.query_row(
-		"SELECT id, name, active_tree, created_at, updated_at FROM trees WHERE active_tree = 1",
-		[],
-		|row| {
-			Ok(Tree {
-				id: row.get(0)?,
-				name: row.get(1)?,
-				active_tree: row.get(2)?,
-				created_at: row.get(3)?,
-				updated_at: row.get(4)?,
-			})
-		}
-	).map_err(|e| e.to_string())?;
+    let tree_data = conn
+        .query_row(
+            "SELECT id, name, active_tree, created_at, updated_at FROM trees WHERE active_tree = 1",
+            [],
+            |row| {
+                Ok(Tree {
+                    id: row.get(0)?,
+                    name: row.get(1)?,
+                    active_tree: row.get(2)?,
+                    created_at: row.get(3)?,
+                    updated_at: row.get(4)?,
+                })
+            },
+        )
+        .optional()
+        .map_err(|e| e.to_string())?;
 
-	Ok(tree_data)
+    Ok(tree_data)
 }

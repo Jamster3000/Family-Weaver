1f873271fb37eee7caf98efb653a8a2ee33e1bf6
jamster3000 <jrbaines.04@gmail.com>
2026-08-21 23:17:45 +0100

    Updated code for test

--- src/components/app/Toolbar.svelte
+++ src/components/app/Toolbar.svelte
@@ -89,8 +89,8 @@
 			],
 		},
 		{
-			id: "test",
-			label: "Test",
+			id: "test2",
+			label: "Test 2",
 			tooltip: "Open settings to change application preferences.",
 			icon: IconSettings,
 			action: () => console.log("open settings"),
